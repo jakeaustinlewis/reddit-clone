ae5049cb8ab9fecd370fc9760940fae952eac34f
Jake Lewis <jakeaustinlewis@gmail.com>
2018-08-13 14:48:09 -0500

    added sort and new post functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,80 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ReactDOM from 'react-dom'
 
-class App extends Component {
+
+function NewPost() {
+  return (
+    <section className="col-3">
+    <button type="button" className="btn btn-primary float-right">New Post</button>
+  </section>
+  );
+}
+
+class Sort extends Component {
   render() {
     return (
-      <div classNameName="App">
-        <header classNameName="App-header">
-          <img src={logo} classNameName="App-logo" alt="logo" />
-          <h1 classNameName="App-title">Welcome to React</h1>
-        </header>
-        <p classNameName="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="row my-4">
+        <section className="col-9 d-flex align-items-center">
+
+          <form className="">
+            <div className="form-group m-0 d-flex align-items-center">
+              <input type="text" className="form-control form-control-sm" id="filter" name="filter" aria-describedby="filter-content" placeholder="Filter"/>
+            </div>
+          </form>
+
+
+
+
+            <div className="dropdown show d-flex align-items-center ml-4">
+              <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Sort
+              </a>
+
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a className="dropdown-item" href="#">Sort By Title</a>
+                <a className="dropdown-item" href="#">Sort By Votes</a>
+                <a className="dropdown-item" href="#">Sort By Date</a>
+              </div>
+            </div>
+        </section>
+          <NewPost />
       </div>
     );
   }
-}
-
-// class Body extends React.Component {
-//   render() { 
-//     return ( 
-//       <section>
-//       <form>
-//         <div class="form-group">
-//           <label for="exampleInputEmail1">Email address</label>
-//           <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-//         </div>
-//       </form>
-//     </section>
-
-//      );
-
-//   }
-
-// }
-class Body extends React.Component {
-  render() {
-    return (
-      <section>
-      <form>
-        <div class="form-group">
-          <label for="filter">Email address</label>
-          <input type="text" className="form-control" id="filter" name="filter" aria-describedby="filter-content" placeholder="Filter"/>
-        </div>
-      </form>
-    </section>
-     
-    );
-  }
   }
 
- 
-
-
-class Navbar extends React.Component {
+class App extends Component {
   render() {
     return(
-      <nav className="navbar navbar-expand-lg navbar-light navBar greenText navBarBorder" style={{backgroundColor: '#f7f7f7'}}>
-        <a className="navbar-brand d-flex align-items-center navBarTitle" href="./">Reddit Clone</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-          </ul>
-        </div>
-      </nav>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light border-bottom" style={{backgroundColor: '#f7f7f7'}}>
+          <a className="navbar-brand d-flex align-items-center navBarTitle" href="./">Reddit Clone</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+            </ul>
+          </div>
+        </nav>
+
+        <section className="container-fliud">
+          <div className="row d-flex justify-content-center">
+            <div className="col-11">
+              <Sort />
+            </div>
+          </div>
+        </section>
+      </div>
     );
-    <Body />
+   
   }
   }
   ReactDOM.render(
-    <Navbar />,
+    <App />,
     document.getElementById('root')
   );
    
-export default Navbar;
+export default App;
